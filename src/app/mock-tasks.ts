91f24f5718127428f0e14f49fd927716f4a3c953
CRISMMXXI <cris.life121@gmail.com>
2022-05-03 12:59:07 -0300

    lista de items

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -2,7 +2,7 @@ import {Task} from "./Task"
 export const TASKS: Task[] = [
     {
         id: 1,
-        text: "terminar primer modulo de Angular",
+        text: "Terminar primer modulo de Angular",
         day: 'Agosto 5 a las 12:00',
         reminder: true
     },
